187a0115d66d9ead70088c55a7d8cc1bd4449a47
Ragoha <ragoha92@naver.com>
2024-02-20 22:21:02 +0900

    [202024] 개발환경세팅

---
+++ src/main/java/com/taeyoung/comet/Oauth2WithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.taeyoung.comet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2WithJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2WithJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taeyoung/comet/Oauth2WithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taeyoung.comet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2WithJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
